2778cd258db80287e014d0d6118350ff8c51418e
hcl_laptop <hcl@laptop>
2025-02-03 17:17:31 +0300

    Add FXThemes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,7 @@
 module org.hcl.oop_database_sem5 {
-    requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-
+    requires com.pixelduke.fxthemes;
 
     opens org.hcl.oop_database_sem5 to javafx.fxml;
     exports org.hcl.oop_database_sem5;
